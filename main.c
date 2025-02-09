ac4ecd121c7549dca89fead2eaeeb7abbbf121bd
ataru <0xataru@proton.me>
2025-02-09 18:27:16 +0200

    output in the terminal

--- main.c
+++ main.c
@@ -1,45 +1,61 @@
-#define STB_IMAGE_IMPLEMENTATION
-#include "./tools/stb_image.h"
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "./tools/stb_image_write.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 
-const char ASCII_CHARS[] = "@%#*+=-:. ";
+#define STB_IMAGE_IMPLEMENTATION
+#include "./tools/stb_image.h"
+
+const char ASCII_CHARS[] =
+    "@$B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/|()1{}[]?-_+~<>i!lI;:,\"^`\'. ";
+
+#define OUTPUT_WIDTH 50
+#define OUTPUT_HEIGHT 50
+
+void resizeImage(const unsigned char *input, int inW, int inH, int channels,
+                 unsigned char *output, int outW, int outH) {
+  float scaleX = (float)inW / outW;
+  float scaleY = (float)inH / outH;
+
+  for (int y = 0; y < outH; y++) {
+    for (int x = 0; x < outW; x++) {
+      int srcX = (int)(x * scaleX);
+      int srcY = (int)(y * scaleY);
+      int idx = (srcY * inW + srcX) * channels;
+
+      int outIdx = (y * outW + x) * 3;
+      output[outIdx] = input[idx];
+      output[outIdx + 1] = (channels > 1) ? input[idx + 1] : input[idx];
+      output[outIdx + 2] = (channels > 1) ? input[idx + 2] : input[idx];
+    }
+  }
+}
 
 void convertToASCII(const unsigned char *image, int width, int height,
-                    int channels, const char *output_file) {
-  FILE *file = fopen(output_file, "w");
-  if (!file) {
-    printf("Could not open file for writing");
-    return;
-  }
+                    int channels) {
+  int asciiLen = sizeof(ASCII_CHARS) - 1;
 
   for (int y = 0; y < height; y++) {
     for (int x = 0; x < width; x++) {
-      unsigned char r = image[(y * width + x) * channels + 0];
-      unsigned char g = image[(y * width + x) * channels + 1];
-      unsigned char b = image[(y * width + x) * channels + 2];
+      int idx = (y * width + x) * channels;
+      unsigned char r = image[idx];
+      unsigned char g = (channels > 1) ? image[idx + 1] : image[idx];
+      unsigned char b = (channels > 1) ? image[idx + 2] : image[idx];
+
       unsigned char brightness = (r + g + b) / 3;
-
-      int index = (brightness * (sizeof(ASCII_CHARS) - 1)) / 255;
-      fputc(ASCII_CHARS[index], file);
+      int asciiIndex = (brightness * (asciiLen - 1)) / 255;
+      putchar(ASCII_CHARS[asciiIndex]);
     }
-    fputs("\n", file);
+    putchar('\n');
   }
-
-  fclose(file);
-  printf("Conversion successful\n");
 }
 
 int main(int argc, char **argv) {
-  if (argc < 3) {
-    printf("Usage: %s <input_file> <output_file>\n", argv[0]);
+  if (argc < 2) {
+    printf("Usage: %s <input_image>\n", argv[0]);
     return 1;
   }
 
   const char *input_file = argv[1];
-  const char *output_file = argv[2];
 
   int width, height, channels;
   unsigned char *image = stbi_load(input_file, &width, &height, &channels, 0);
@@ -48,8 +64,20 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  convertToASCII(image, width, height, channels, output_file);
+  int newW = OUTPUT_WIDTH;
+  int newH = OUTPUT_HEIGHT;
 
+  unsigned char *resizedImage = (unsigned char *)malloc(newW * newH * 3);
+  if (!resizedImage) {
+    printf("Memory allocation failed\n");
+    stbi_image_free(image);
+    return 1;
+  }
+
+  resizeImage(image, width, height, channels, resizedImage, newW, newH);
+  convertToASCII(resizedImage, newW, newH, 3);
+
+  free(resizedImage);
   stbi_image_free(image);
   return 0;
 }
